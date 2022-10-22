d7a31f9ef220b140a3e81e6bddaacd93828dc660
Rakshit Agarwal <rakshitagarwal@users.noreply.github.com>
2022-10-22 16:05:10 +0530

    Delete Documents/github/InnovatorIsland directory

--- Documents/github/InnovatorIsland/5-park-stats/2-simulator/sam-app/simulatorFunction/local.js
+++
@@ -1,8 +0,0 @@
-/*! Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *  SPDX-License-Identifier: MIT-0
- */
-
- // For local testing
-
-const { main } = require('./simulation')
-main()
